a9bd977581cdd614d84a2cc8adb32e57d895b7e7
Izaura <izahonorato@gmail.com>
2021-07-07 21:05:25 -0300

    Componentização dos inputs de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import logo from '../assets/icons/logo.svg'; //importando o arquivo svg e jogando numa variável chamada logo
 import mail from '../assets/icons/mail.svg';
 import lock from '../assets/icons/lock.svg';
+import { Input } from '../components/Input';
 
 export const Login = () => {
 
@@ -28,17 +29,25 @@ export const Login = () => {
                 className="logo"
             />
             <form>
-                <div className="input">
-                    <img src={mail} alt="icone e-mail"/>
-                    <input type="text" name="login" placeholder="Informe seu e-mail" 
-                    value={login} onChange={evento => setLogin(evento.target.value)}></input>
-                </div>
-                <div className="input">
-                    <img src={lock} alt="icone senha"/>
-                    <input type="password" name="senha" placeholder="Informe sua senha"
-                    value={senha} onChange={evento => setSenha(evento.target.value)}></input>
-                </div>
-                
+                <Input
+                    srcImg={mail} 
+                    altImg="Icone email"
+                    inputType="text"
+                    inputName="login"
+                    inputPlaceholder="Informe seu e-mail" 
+                    value={login}
+                    setValue={setLogin}
+                />
+                <Input
+                    srcImg={lock} 
+                    altImg="Icone senha"
+                    inputType="password"
+                    inputName="senha"
+                    inputPlaceholder="Informe sua senha" 
+                    value={senha}
+                    setValue={setSenha}
+                />
+                               
                 <button onClick={executaLogin} disabled={isLoading === true}>{isLoading === true ? '...Carregando' : 'Entrar'}</button>
             </form>
         </div>
